358b55f73456ff11694fe8f8a3da36cd24f55c59
xcodemonk <xcodemonk.ios@gmail.com>
2023-05-21 19:13:39 +0530

    the view exmples and properties

--- project-3/ContentView.swift
+++ project-3/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useReadText = false
+//    var motto1 : some View { Text("Draco dormers") }
+//    let motto2 = Text("naoko taoaal")
+//    var spells : some View{
+//        Group{
+//            Text("Limos")
+//            Text("Obligate")
+//        }
+//    }
+//    @ViewBuilder var spells : some View{
+//               Text("Limos")
+//               Text("Obligate")
+//       }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            //            Image(systemName: "globe")
+            //                .imageScale(.large)
+            //                .foregroundColor(.accentColor)
+            //            Text("Hello, world!")
+            //                .frame(maxWidth: .infinity, maxHeight:.infinity)
+            //                .background(.red)
+            
+            //            Button("Hello, World!"){
+            //                print(type(of: self.body))
+            //            }
+            //            .background(.red)
+            //            .frame(width: 200,height: 200)
+            
+            //            Button("Hello, World!"){
+            //                useReadText.toggle()
+            //                      }
+            //            .foregroundColor(useReadText ? .red : .orange)
+            //
+            //            Text("Text 1")
+            //                .font(.largeTitle)
+            //            Text("Text 2")
+            //            Text("Text 3")
+            //            Text("Text 4")
+            
+//            motto1
+//                .foregroundColor(.red)
+//            motto2
+//                .foregroundColor(.blue)
+         //   spells
+            
+            Text("First")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(.blue)
+                .clipShape(Capsule())
+            Text("Second")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(.blue)
+                .clipShape(Capsule())
         }
-        .padding()
+     //   .font(.title)
+        
+       
     }
 }
 
