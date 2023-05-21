d5826089092229ad8cd9cded45b722052d3de671
xcodemonk <xcodemonk.ios@gmail.com>
2023-05-21 13:49:04 +0530

    first comment

---
+++ project-3/project_3App.swift
@@ -0,0 +1,17 @@
+//
+//  project_3App.swift
+//  project-3
+//
+//  Created by Yogesh on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct project_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
